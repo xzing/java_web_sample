774662d601773f53712df403b57c78c53785df93
zing <x1zing@126.com>
2019-09-02 20:18:35 +0800

    Optional 测试

---
+++ demo004_java8/src/test/java/TestOptional.java
@@ -0,0 +1,19 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
+
+
+/**
+ * create at     2019/9/2 6:57 下午
+ *
+ * @author zing
+ * @version 0.0.1
+ */
+public class TestOptional {
+    @Test
+    public void testOptional() {
+        Optional<String> demo = Optional.empty();
+        demo.ifPresent(System.out::println);
+        System.out.println(demo.orElseGet(() -> "N"));
+    }
+}
