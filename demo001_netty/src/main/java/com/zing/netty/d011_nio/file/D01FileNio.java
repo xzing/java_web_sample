1a5b8b2ba77da2ace8171fcf1ea42241b1bdd146
Zing <pzl144@zcsmart.com>
2021-02-23 18:23:45 +0800

    NIO of File

--- demo001_netty/src/main/java/com/zing/netty/d011_nio/file/D01FileNio.java
+++ demo001_netty/src/main/java/com/zing/netty/d011_nio/file/D01FileNio.java
@@ -10,8 +10,8 @@ import java.nio.ByteBuffer;
 import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.StandardOpenOption;
 import java.util.UUID;
+import java.util.stream.Stream;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
 
@@ -42,34 +42,67 @@ public class D01FileNio {
                 }
             }
         }
+        //
+        //
+        // // 限制空间(非NIO方式)
+        // ByteBuffer b = ByteBuffer.allocate(4096);
+        // try (FileOutputStream outputStream = new FileOutputStream(file, true)) {
+        //     FileChannelImpl channel = (FileChannelImpl) outputStream.getChannel();
+        //     for (int i = 0; i < 5000; i++) {
+        //         String data = UUID.randomUUID().toString() + "\n";
+        //         b.put(data.getBytes(UTF_8));
+        //         b.flip();
+        //         while (true) {
+        //             if (channel.write(b) == 0) {
+        //                 b.clear();
+        //                 break;
+        //             }
+        //         }
+        //     }
+        // }
+        //
+        // // 啥都不限制
+        // Path path = FileSystems.getDefault().getPath("./", "demo.log");
+        // for (int i = 0; i < 5000; i++) {
+        //     String data = UUID.randomUUID().toString() + "\n";
+        //     Files.write(path, data.getBytes(UTF_8), StandardOpenOption.APPEND,
+        //             StandardOpenOption.CREATE, StandardOpenOption.SYNC, StandardOpenOption.WRITE);
+        // }
 
-
-        // 限制空间(非NIO方式)
-        ByteBuffer b = ByteBuffer.allocate(4096);
-        try (FileOutputStream outputStream = new FileOutputStream(file, true)) {
-            FileChannelImpl channel = (FileChannelImpl) outputStream.getChannel();
-            for (int i = 0; i < 5000; i++) {
-                String data = UUID.randomUUID().toString() + "\n";
-                b.put(data.getBytes(UTF_8));
-                b.flip();
-                while (true) {
-                    if (channel.write(b) == 0) {
-                        b.clear();
-                        break;
-                    }
-                }
-            }
-        }
-
-        // 啥都不限制
-        Path path = FileSystems.getDefault().getPath("./", "demo.log");
-        for (int i = 0; i < 5000; i++) {
-            String data = UUID.randomUUID().toString() + "\n";
-            Files.write(path, data.getBytes(UTF_8), StandardOpenOption.APPEND,
-                    StandardOpenOption.CREATE, StandardOpenOption.SYNC, StandardOpenOption.WRITE);
-        }
 
         // 大文件使用Map
-        // MappedByteBuffer
+        // RandomAccessFile aFile = new RandomAccessFile("test.txt", "r");
+        //
+        // FileChannel inChannel = aFile.getChannel();
+        // MappedByteBuffer buffer = inChannel.map(FileChannel.MapMode.READ_ONLY, 0, inChannel.size());
+        //
+        // buffer.load();
+        // for (int i = 0; i < buffer.limit(); i++) {
+        //     System.out.print((char) buffer.get());
+        // }
+        // buffer.clear(); // do something with the data and clear/compact it.
+        //
+        // inChannel.close();
+        // aFile.close();
+// --------------------------------------------------------
+        // 读取
+        // try (RandomAccessFile randomAccessFile = new RandomAccessFile("./demo.log", "r")) {
+        //     FileChannelImpl channel = (FileChannelImpl) randomAccessFile.getChannel();
+        //     ByteBuffer readBuffer = ByteBuffer.allocate(1024);
+        //     while (channel.read(readBuffer) > 0) {
+        //         readBuffer.flip();
+        //         System.out.print(new String(readBuffer.array(), UTF_8));
+        //         readBuffer.clear();
+        //     }
+        // }
+
+
+        // 读取2
+        Path path = FileSystems.getDefault().getPath("./demo.log");
+        try (Stream<String> stream = Files.lines(path)) {
+            stream.forEach(System.out::println);
+        }
+
+
     }
 }
